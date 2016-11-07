0e91a55ff2472a649fc6195fbb98e2c6bf3ebbd6
唐洁 <13799369482@139.com>
2016-11-07 14:19:40 +0800

    NettyRPC 2.0 pojo rpc demo by tangjie
    NettyRPC 2.0 pojo rpc demo by tangjie

---
+++ NettyRPC 2.0/main/java/com/newlandframework/rpc/services/pojo/Person.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2016 Newland Group Holding Limited
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.newlandframework.rpc.services.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author tangjie<https://github.com/tang-jie>
+ * @filename:Person.java
+ * @description:Person功能模块
+ * @blogs http://www.cnblogs.com/jietang/
+ * @since 2016/11/7
+ */
+public class Person implements Serializable {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return String.format("Person <<id:%d name:%s age:%d>>", id, name, age);
+    }
+}
+
---
+++ NettyRPC 2.0/test/java/com/newlandframework/test/PojoCallTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2016 Newland Group Holding Limited
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.newlandframework.test;
+
+import com.newlandframework.rpc.services.PersonManage;
+import com.newlandframework.rpc.services.pojo.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author tangjie<https://github.com/tang-jie>
+ * @filename:PojoCallTest.java
+ * @description:PojoCallTest功能模块
+ * @blogs http://www.cnblogs.com/jietang/
+ * @since 2016/11/7
+ */
+public class PojoCallTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:rpc-invoke-config-client.xml");
+
+        PersonManage manage = (PersonManage) context.getBean("personManage");
+
+        Person p = new Person();
+        p.setId(20150811);
+        p.setName("XiaoHaoBaby");
+        p.setAge(1);
+
+        int result = manage.save(p);
+
+        System.out.println("call pojo rpc result:" + result);
+
+        context.destroy();
+    }
+}
+
